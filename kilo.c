7caf2eedd54ba51812b2fdb366cab3256c78a31a
sbui056 <119729517+sbui056@users.noreply.github.com>
2025-07-31 17:25:52 -0700

    prototype del key for later

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
     HOME_KEY,
     END_KEY,
     PAGE_UP,
@@ -82,6 +83,7 @@ int editorReadKey() {
                 if (seq[2] == '~') {
                     switch (seq[1]) {
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
@@ -250,7 +252,7 @@ void editorProcessKeypress() {
         case HOME_KEY:
             E.cx = 0;
             break;
-            
+
         case END_KEY:
             E.cx = E.screencols - 1;
             break;

92a7bafcf5d174203473940f2699419efb2b5d1a
sbui056 <119729517+sbui056@users.noreply.github.com>
2025-08-04 18:16:17 -0700

    finish all text editing operations planned

--- kilo.c
+++ kilo.c
@@ -211,10 +211,13 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+    if (at < 0 || at > E.numrows) return;
 
-    int at = E.numrows;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at],
+            sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -269,12 +272,27 @@ void editorRowDelChar(erow *row, int at) {
 }
 
 /*** editor operations ***/
-void editorInserChar(int c) {
+void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar() {
@@ -328,7 +346,7 @@ void editorOpen(char* filename) {
         while (linelen > 0 && (line[linelen - 1] == '\n' ||
                                 line[linelen - 1] == '\r'))
             linelen--;
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -542,7 +560,7 @@ void editorProcessKeypress() {
 
     switch (c) {
         case '\r':
-            /*todo*/
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
@@ -610,7 +628,7 @@ void editorProcessKeypress() {
             break;
 
         default:
-            editorInserChar(c);
+            editorInsertChar(c);
             break;
     }
     quit_times = KILO_QUIT_TIMES;
